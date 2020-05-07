4d8592d9d8b24314b611e4ff5ebe1f19623e627d
Edward M <edwardtmuniz@gmail.com>
2020-05-06 21:04:20 -0700

    working!

--- MPGCalculatorController.java
+++ MPGCalculatorController.java
@@ -1,16 +1,30 @@
 import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class MPGCalculatorController implements Initializable {
+
+    @FXML
+    private TextField gallonsTextField = new TextField();
+    @FXML
+    private TextField milesTextField = new TextField();
+    @FXML
+    private TextField mpgTextField = new TextField();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("View is now loaded!");
     }
 
-    public void calculateButtonPressed() {
-        System.out.println("hello");
+    public void calculateButtonPressed(ActionEvent event) {
+        double miles = Integer.parseInt(milesTextField.getText());
+        double gallons = Integer.parseInt(gallonsTextField.getText());
+        double result = miles / gallons;
+
+        mpgTextField.setText(Double.toString(result));
     }
 
 }
