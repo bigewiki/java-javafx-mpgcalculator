b48bf25261ddd09f66d250170e6f8a84250ed05e
Edward M <edwardtmuniz@gmail.com>
2020-05-06 20:25:31 -0700

    got the controller and fxml all hooked up!

--- HelloFX.java
+++ HelloFX.java
@@ -1,23 +1,21 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class HelloFX extends Application {
 
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 
-    public static void main(String[] args) {
-        launch();
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("MPGCalculator.fxml"));
+        primaryStage.setTitle("Hello JavaFX");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
     }
 
 }
--- MPGCalculatorController.java
+++ MPGCalculatorController.java
@@ -1,5 +1,16 @@
-public class MPGCalculatorController {
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MPGCalculatorController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("View is now loaded!");
+    }
+
     public void calculateButtonPressed() {
         System.out.println("hello");
     }
+
 }
